b6059c65d3f64e961f688a0649e5f545d5cd0a2a
Ashish-sinh <ashishbaraiya.ce21@sltiet.edu.in>
2024-04-23 14:48:22 +0530

    commit-1-added

---
+++ x/app.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+print("Hellow World")
+print("Below Status") 
+
+
+
+print('This is the Again Uploaded Data and i wanted to Check') 
+
+
+print("this Modification made by me in outside of of git-repo folder and for commit-1") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
